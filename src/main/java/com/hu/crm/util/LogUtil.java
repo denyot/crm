0ae5e46565751d681ce975c64b1940c03b142225
denyot <823300635@qq.com>
2017-09-09 15:36:34 +0800

    完善aop日志，注解方式

--- src/main/java/com/hu/crm/util/LogUtil.java
+++ src/main/java/com/hu/crm/util/LogUtil.java
@@ -6,16 +6,29 @@ import com.hu.crm.domain.Employee;
 import com.hu.crm.domain.Log;
 import com.hu.crm.service.ILogService;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
+@Aspect
+@Component
 public class LogUtil {
     @Autowired
     private ILogService logService;
+
+    @Pointcut("execution(* com.hu.crm.service.*Service.*(..))")
+    public void logPointcut() {
+
+    }
+
+    @After("logPointcut()")
     public void writeLog(JoinPoint joinPoint) {
-        if(joinPoint.getTarget() instanceof ILogService){
+        if (joinPoint.getTarget() instanceof ILogService) {
             return;
         }
         Log log = new Log();
